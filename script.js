52d4479f8992f21834847dcc789e55935b9979f2
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-10-15 09:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+	let newRow = document.createElement("tr");
+	newRow.innerHTML=`
+		<td>New Cell1</td>
+		<td>New Cell2</td>
+	`;
+	let table = document.getElementById("sampleTable");
+	table.appendChild(newRow);
+
   
 }
